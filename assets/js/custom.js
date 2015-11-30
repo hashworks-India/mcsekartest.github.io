b64021ad0b8d833339011524060871b1b07cd7c7
chandrasekar M <mcsekargct@gmail.com>
2015-11-30 19:40:03 +0530

    Added hashworks
    Added hashworks

---
+++ assets/js/custom.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+
+
+    $('.toggle').click(function (event) {
+        event.preventDefault();
+        var target = $(this).attr('href');
+        $(target).toggleClass('hidden show');
+
+});
+});
+
