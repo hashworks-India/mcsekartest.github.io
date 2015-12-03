8996a3c0adc2b90fb9114c191256ec8b457a25c0
PalaniKamaraj <palani13kamaraj@yahoo.com>
2015-12-03 13:50:43 +0530

    UI/UX functionality is updated in IT service page

--- _site/assets/js/custom.js
+++ _site/assets/js/custom.js
@@ -9,44 +9,36 @@ $(document).ready(function(){
 
 });
 // js for carousel platform integeration
-    $('#osb-testimonial-first .carousel-indicators li').on('click', function() {
-  $('.carousel-osb').carousel(parseInt(this.getAttribute('data-to')));
-});
-     $('#ms-testimonial-first .carousel-indicators li').on('click', function() {
-  $('.carousel-ms ').carousel(parseInt(this.getAttribute('data-to')));
-});
-      $('#ad-testimonial-first .carousel-indicators li').on('click', function() {
-  $('.carousel-ad ').carousel(parseInt(this.getAttribute('data-to')));
-});
-       $('#etl-testimonial-first .carousel-indicators li').on('click', function() {
-  $('.carousel-etl ').carousel(parseInt(this.getAttribute('data-to')));
-});
-// js for carousel engineering practices
-        $('#ep1-testimonial-first .carousel-indicators li').on('click', function() {
-  $('.carousel-ep1').carousel(parseInt(this.getAttribute('data-to')));
-});
-     $('#ep2-testimonial-first .carousel-indicators li').on('click', function() {
-  $('.carousel-ep2 ').carousel(parseInt(this.getAttribute('data-to')));
-});
-      $('#ep3-testimonial-first .carousel-indicators li').on('click', function() {
-  $('.carousel-ep3 ').carousel(parseInt(this.getAttribute('data-to')));
-});
-       $('#ep4-testimonial-first .carousel-indicators li').on('click', function() {
-  $('.carousel-ep4 ').carousel(parseInt(this.getAttribute('data-to')));
-});
-// js for carousel mobility/bigdata
-        $('#mb1-testimonial-first .carousel-indicators li').on('click', function() {
-  $('.carousel-mb1').carousel(parseInt(this.getAttribute('data-to')));
-});
-     $('#mb2-testimonial-first .carousel-indicators li').on('click', function() {
-  $('.carousel-mb2 ').carousel(parseInt(this.getAttribute('data-to')));
-});
-      $('#mb3-testimonial-first .carousel-indicators li').on('click', function() {
-  $('.carousel-mb3 ').carousel(parseInt(this.getAttribute('data-to')));
-});
-       $('#mb4-testimonial-first .carousel-indicators li').on('click', function() {
-  $('.carousel-mb4 ').carousel(parseInt(this.getAttribute('data-to')));
-});
+  
+  
+
+
+      // it-services
+
+        // testimonial-carousel
+          $('.testimonial-carousel .carousel-indicators li').click(function(){
+              $(this).parents('.testimonial-carousel').find('.carousel').carousel(parseInt(this.getAttribute('data-to')));
+          });
+        // End of testimonial-carousel
+
+        // ui-ux tab
+          $('.hw-ui-ux-carousel .carousel').carousel({
+            interval: 1000,
+              pause: true
+          });
+          $(window).load(function () {
+              $('.hw-ui-ux-carousel .carousel').carousel("pause");
+          });
+          
+          $('.play').click(function () {
+              $(this).parents('.hw-view-project').siblings('.carousel').carousel('cycle');
+          });
+          $('.pause').click(function () {
+              $(this).parents('.hw-view-project').siblings('.carousel').carousel('pause');
+          });
+        // End of ui-ux tab
+
+      // End of it-services
 
 
 
