1463e32663f0c46d3d2bc6616789c445d6ef426f
unknown <founders@hashworks.co>
2016-01-08 17:21:01 +0530

    google map

--- assets/js/googlemap.js
+++ assets/js/googlemap.js
@@ -37,17 +37,34 @@
       // initial load google map location
         var map;
           function initMap() {
-            var myLatLng = {lat: 12.9093324, lng: 77.6403711};
+            var locations = [
+      ['Bondi Beach', 12.9093324, 77.6403711]
+    ];
 
-            map = new google.maps.Map(document.getElementById('map'), {
-              zoom: 10,
-              center: myLatLng
-            });
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 10,
+      center: new google.maps.LatLng(12.9093324, 77.6403711),
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
 
-            var marker = new google.maps.Marker({
-            position: myLatLng,
-            map: map
-          });
+    var infowindow = new google.maps.InfoWindow();
+
+    var marker, i;
+
+    for (i = 0; i < locations.length; i++) {  
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+        map: map
+      });
+
+      google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        return function() {
+          infowindow.setContent(locations[i][0]);
+          infowindow.open(map, marker);
+        }
+      })(marker, i));
+    }
+
 
 
           }
@@ -55,7 +72,6 @@
 
       // End of initial load google map location
 
-      
 
       $(document).ready(function() {
 
@@ -104,5 +120,49 @@
           calculateRoute($("#from").val(), $("#to").val());
         });
         //End of after clicking the submit button
+                    
+      // Info Window Content
+      var infoWindowContent = [
+          ['<div class="info_content">' +
+          '<div class="row">' +
+          '<div class="col-lg-6">'+
+            '<div class="map-img center-align">'+
+              '<img id="link" onclick="showmap()" src="../img/car-list/car1.png" alt="">'+
+            '</div>'+
+          '</div>'+
+          '<div class="col-lg-6 map-cont-border">'+
+            '<div class="map-desc">'+
+              '<h1>Maruti Swift</h1>'+
+              '<span class="star-rating"> <i class="fa fa-star fa-enable fa-lg"></i> <i class="fa fa-star fa-enable fa-lg"></i> <i class="fa fa-star fa-enable fa-lg"></i> <i class="fa fa-star fa-disable fa-lg"></i> <i class="fa fa-star fa-disable fa-lg"></i> </span><span class="comments"> <i class="vsvg v-comment-icon"></i> 05 </span>'+
+                '<div class="price">Rs. 2,500</div>'+
+            '</div>'+
+          '</div>'+
+            '</div>'+
+              '</div>'],
+          ['<div class="info_content">' +
+          '<div class="row">' +
+          '<div class="col-lg-6">'+
+            '<div class="map-img center-align">'+
+              '<img id="link" onclick="showmap()" src="../img/car-list/car1.png" alt="">'+
+            '</div>'+
+          '</div>'+
+          '<div class="col-lg-6 map-cont-border">'+
+            '<div class="map-desc">'+
+              '<h1>Maruti Swift</h1>'+
+              '<span class="star-rating"> <i class="fa fa-star fa-enable fa-lg"></i> <i class="fa fa-star fa-enable fa-lg"></i> <i class="fa fa-star fa-enable fa-lg"></i> <i class="fa fa-star fa-disable fa-lg"></i> <i class="fa fa-star fa-disable fa-lg"></i> </span><span class="comments"> <i class="vsvg v-comment-icon"></i> 05 </span>'+
+                '<div class="price">Rs. 2,500</div>'+
+            '</div>'+
+          '</div>'+
+            '</div>'+
+              '</div>']
+      ];
+          var infoWindow = new google.maps.InfoWindow(), marker, i;
+          // Allow each marker to have an info window    
+          google.maps.event.addListener(marker, 'mouseover', (function(marker, i) {
+              return function() {
+                  infoWindow.setContent(infoWindowContent[i][0]);
+                  infoWindow.open(map, marker);
+              }
+          })(marker, i));
 
       });
