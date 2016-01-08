b86b5af5457af5b5a5fbc5ed26ee8ef848c3e23a
sasanka112 <st970698@gmail.com>
2016-01-08 13:42:06 +0530

    form padding correction

--- assets/js/form/email3.js
+++ assets/js/form/email3.js
@@ -1,4 +1,5 @@
-   var value;
+        var value;
+        var inputNo=1;
         var handleFileSelect = function(evt) {
         var files = evt.target.files;
         var file = files[0];
@@ -15,7 +16,6 @@
     };
 
     if (window.File && window.FileReader && window.FileList && window.Blob) {
-        document.getElementById('file-upload').addEventListener('change', handleFileSelect, false);
     } else {
         alert('The File APIs are not fully supported in this browser.');
     }
@@ -23,79 +23,94 @@
     function submitForm(e)
     {
                 e.preventDefault();
-                var fileName = $('#file-upload').val();
-                var fileExtsn=fileName.substr( (fileName.lastIndexOf('.')));
+                var fileName;
                 var filter_email = /^[\w\-\.\+]+\@[a-zA-Z0-9\.\-]+\.[a-zA-z0-9]{2,4}$/;
                 var filter_contact = /^[0-9-+]+$/;
-                var name = $("#name").val();
-                var email = $("#email").val();
-                var subject = $("#subject").val();
-                var contact = $("#phone").val();
-                var message = $("#message").val();
+                var name = $(".input_name").val();
+                var email = $(".input_email").val();
+                var subject = $(".input_subject").val();
+                var contact = $(".input_phone").val();
+                var message = $(".input_message").val();
                 var flag=0; 
+                if(inputNo==1)
+                {
+                    fileName = $('.input_file-upload').val();
+                }
+                else if(inputNo==2)
+                {
+                    fileName = $('.input_file-upload2').val();
+                }
+                else if(inputNo==3)
+                {
+                    fileName = $('.input_file-upload3').val();
+                }
+                var fileExtsn=fileName.substr( (fileName.lastIndexOf('.')));
                 if(value == null) {
                     flag=1;
-                    $('#file-detail').val("Please upload your resume");
+                    $('.input_file-detail').val("Please upload your resume");
+                    $('.input_file-detail2').val("Please upload your resume");
+                    $('.input_file-detail3').val("Please upload your resume");
                 }
-
                 if (name == "" || email == "" || subject == "Subject" || contact == "" || message == "")
                 {
                     flag=1;
                     if (name == "")
                         {
-                            $("#name").parent().parent().css("border", "1px solid red");
-                            $("#name").attr("placeholder", "Please enter your Name ");
-                            placeholdAnimate("#name");
+                            $(".input_name").parent().parent().css("border", "1px solid red");
+                            $(".input_name").attr("placeholder", "Please enter your Name ");
+                            placeholdAnimate(".input_name");
                         }
                     if (email == "")
                         {
-                            $("#email").parent().parent().css("border", "1px solid red");
-                            $("#email").attr("placeholder", "Please enter your Email ");
-                            placeholdAnimate("#email");
+                            $(".input_email").parent().parent().css("border", "1px solid red");
+                            $(".input_email").attr("placeholder", "Please enter your Email ");
+                            placeholdAnimate(".input_email");
                         }
                     if (subject == "Subject")
                         {
-                            $("#subject").parent().parent().css("border", "1px solid red");
+                            $(".input_subject").parent().parent().css("border", "1px solid red");
                         }
                     if (contact == "")
                         {
-                            $("#phone").parent().parent().css("border", "1px solid red");
-                            $("#phone").attr("placeholder", "Please enter your conatct no");
-                            placeholdAnimate("#phone");
+                            $(".input_phone").parent().parent().css("border", "1px solid red");
+                            $(".input_phone").attr("placeholder", "Please enter your conatct no");
+                            placeholdAnimate(".input_phone");
                         }   
                     if (message == "")
                         {
-                            $("#message").parent().parent().css("border", "1px solid red");
-                            $("#message").attr("placeholder", "Please enter your message");
-                            placeholdAnimate("#message");
-                        }   
+                            $(".input_message").parent().parent().css("border", "1px solid red");
+                            $(".input_message").attr("placeholder", "Please enter your message");
+                            placeholdAnimate(".input_message");
+                        }  
+                        return false; 
                 }
                 else
                 {   
                     if(!filter_email.test(email))
                         {
                             flag=1;
-                            $("#email").val('');
-                            $("#email").parent().parent().css("border", "1px solid red");
-                            $("#email").attr("placeholder", "Please enter your Email correctly");
-                            placeholdAnimate("#email");
+                            $(".input_email").val('');
+                            $(".input_email").parent().parent().css("border", "1px solid red");
+                            $(".input_email").attr("placeholder", "Please enter your Email correctly");
+                            placeholdAnimate(".input_email");
                         }
                     if(!filter_contact.test(contact))
                         {
                             flag=1;
-                            $("#phone").val('');
-                            $("#phone").parent().parent().css("border", "1px solid red");
-                            $("#phone").attr("placeholder", "Please enter your conatct no correctly");
-                            placeholdAnimate("#phone");
+                            $(".input_phone").val('');
+                            $(".input_phone").parent().parent().css("border", "1px solid red");
+                            $(".input_phone").attr("placeholder", "Please enter your conatct no correctly");
+                            placeholdAnimate(".input_phone");
                         }
                     else if((contact.length)>10||(contact.length)<9)
                         {
                             flag=1;
-                            $("#phone").val('');
-                            $("#phone").parent().parent().css("border", "1px solid red");
-                            $("#phone").attr("placeholder", "Please enter your conatct no correctly");
-                            placeholdAnimate("#phone");
+                            $(".input_phone").val('');
+                            $(".input_phone").parent().parent().css("border", "1px solid red");
+                            $(".input_phone").attr("placeholder", "Please enter your conatct no correctly");
+                            placeholdAnimate(".input_phone");
                         }
+                        return false;
                 }   
 
                 if(flag==0)
@@ -132,47 +147,61 @@
                             })
                             .done(function(response) {
                                 alert('Your message has been sent. Thank you!'); // show success message
-                                $("#name").val(''); // reset field after successful submission
-                                $("#email").val(''); // reset field after successful submission
-                                $("#subject").val('Subject'); // reset field after successful submission
-                                $("#phone").val(''); // reset field after successful submission
-                                $("#message").val(''); // reset field after successful submission
-                                $('#file-upload').val('');// reset field after successful submission
+                                $(".input_name").val(''); // reset field after successful submission
+                                $(".input_email").val(''); // reset field after successful submission
+                                $(".input_subject").val('Subject'); // reset field after successful submission
+                                $(".input_phone").val(''); // reset field after successful submission
+                                $(".input_message").val(''); // reset field after successful submission
                             })
                             .fail(function(response) {
                                 alert('Error sending message.');
                             });
                             return false;
                         }
-        
     
     } //end of submitForm function
 
     function onClickText(inputElement){
-    $(inputElement).parent().parent().css("border", "1px solid #0AB4C5");
+    $(inputElement).parent().parent().css("border", "1px solid .input_0AB4C5");
     }   
-    $('#name').click(function(){
+    $('.input_name').click(function(){
         onClickText(this);
     });
-    $('#email').click(function(){
+    $('.input_email').click(function(){
         onClickText(this);
     });
-    $('#subject').click(function(){
+    $('.input_subject').click(function(){
         onClickText(this);
     });
-    $('#phone').click(function(){
+    $('.input_phone').click(function(){
         onClickText(this);
     });
-    $('#message').click(function(){
+    $('.input_message').click(function(){
         onClickText(this);
     });
 
-    $('#file-upload').on('change',function(){
-        var fileName = $('#file-upload').val();
-        $('#file-detail').val(fileName);
+    $('.input_file-upload').on('change',function(){
+        inputNo=1;
+        document.querySelector(".input_file-upload").addEventListener('change', handleFileSelect, false);
+        var fileName = $('.input_file-upload').val();
+        $('.input_file-detail').val(fileName);
     });
 
 
+        $('.input_file-upload2').on('change',function(){
+        inputNo=2;
+        document.querySelector(".input_file-upload2").addEventListener('change', handleFileSelect, false);
+        var fileName = $('.input_file-upload2').val();
+        $('.input_file-detail2').val(fileName);
+    });
+            $('.input_file-upload3').on('change',function(){
+        inputNo=3;
+        document.querySelector(".input_file-upload3").addEventListener('change', handleFileSelect, false);
+        var fileName = $('.input_file-upload3').val();
+        $('.input_file-detail3').val(fileName);
+    });
+
+/* for placeholder of textbox animation*/
     function placeholdAnimate(element){
     var arr = new Array();
     var str=$(element).attr('placeholder');
